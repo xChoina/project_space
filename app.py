6fe092303e451c89e2dc111e174ee3996f779ff6
xChoina <barti5416@gmail.com>
2026-05-31 21:05:43 +0200

    dodanie konstelacji, aktuzalizacja bazy

--- app.py
+++ app.py
@@ -15,14 +15,20 @@ def pobierz_dane_z_bazy():
 
     df_planety = pd.read_sql_query("SELECT * FROM Planety", conn, index_col='nazwa')
     df_gwiazdy = pd.read_sql_query("SELECT * FROM Gwiazdy", conn, index_col='hip_id')
+    df_konst = pd.read_sql_query("SELECT * FROM Konstelacje ORDER BY nazwa, kolejnosc", conn)
     conn.close()
     kolory = df_planety['kolor'].to_dict()
     rozmiary = df_planety['rozmiar'].to_dict()
     okresy = df_planety['okres'].to_dict()
     nazwy_gwiazd = df_gwiazdy['nazwa'].to_dict()
-    return kolory, rozmiary, okresy, nazwy_gwiazd
 
-kolor_planet, rozmiar_planet, okres_planet, nazwy_gwiazd = pobierz_dane_z_bazy()
+    konstelacje = {}
+    for nazwa, grupa in df_konst.groupby('nazwa'):
+        konstelacje[nazwa] = grupa['hip_id'].tolist()
+
+    return kolory, rozmiary, okresy, nazwy_gwiazd, konstelacje
+
+kolor_planet, rozmiar_planet, okres_planet, nazwy_gwiazd, konstelacje = pobierz_dane_z_bazy()
 #wyszukanie lokalizacji
 st.sidebar.header("Twoja lokalizacja")
 wpisane_miasto = st.sidebar.text_input("Wpisz miasto:", "Warszawa")
@@ -57,11 +63,14 @@ if chmury is not None:
 else:
     st.sidebar.write("Bezchmurne niebo")
 
+st.sidebar.markdown("---")
+pokaz_kon = st.sidebar.toggle("Pokaż linie konstelacji", value=True)
+
 @st.cache_data
 def zaladuj_gwiazdy():
     with load.open(hipparcos.URL) as f:
         df = hipparcos.load_dataframe(f)
-        jasne_df = df[df['magnitude'] <= 2.5]
+        jasne_df = df[df['magnitude'] <= 3.5]
         return jasne_df
 df_gwiazdy = zaladuj_gwiazdy()
 
@@ -150,6 +159,8 @@ czas_lokalny = t.utc_datetime().astimezone()
 #2D_mapa_nieba
 with tab_2d:
     st.subheader(f"Mapa nieba czasu lokalnego: ({czas_lokalny.strftime('%Y-%m-%d %H:%M')} UTC)")
+    kol_mapa, kol_tab = go.Figure(), None
+    col1, col2 = st.columns([2,1])
     latidue = lat
     longitude = lon
     obs = earth + wgs84.latlon(latidue, longitude)
@@ -159,14 +170,23 @@ with tab_2d:
     obiekty_2d = {k: v for k, v in targets.items() if k != 'Ziemia'}
     obiekty_2d['Słońce'] = planets['sun']
     obiekty_2d['Księżyc'] = planets['moon']
+    dane_tabela = []
 
     for name, obj in obiekty_2d.items():
         astrometric = obs.at(t).observe(obj).apparent()
         alt, az, distance = astrometric.altaz()
+
         if alt.degrees > 0:
             widoczne += 1
+            dist_au = distance.au
+            dist_km = dist_au*149597871
+            dane_tabela.append({
+                "Obiekt": name,
+                "Dystans(AU)": f"{dist_au:.4f}",
+                "Dystans (km)": f"{dist_km:.0f}"
+            })
             fig_2d.add_trace(go.Scatterpolar(
-                r=[90-alt.degrees],
+                r=[90 - alt.degrees],
                 theta=[az.degrees],
                 mode='markers+text',
                 text=[name],
@@ -184,10 +204,15 @@ with tab_2d:
         ),
         paper_bgcolor='black',font=dict(color='white'),height=600
     )
-    if widoczne == 0:
-        st.warning("Brak obiektów widocznych nad tobą")
-    else:
-        st.plotly_chart(fig_2d, use_container_width=True, key="wykres_2d")
+    with col1:
+        if widoczne == 0:
+            st.warning("Brak obiektów widocznych nad tobą")
+        else:
+            st.plotly_chart(fig_2d, use_container_width=True, key="wykres_2d")
+    with col2:
+        st.markdown("Odległość od Ziemi")
+        df_odl = pd.DataFrame(dane_tabela)
+        st.dataframe(df_odl,hide_index=True)
 
 #MAPA2D_tylko_gwiazdy
 with tab_2d_gwiazdy:
@@ -203,14 +228,24 @@ with tab_2d_gwiazdy:
     maska_widocznosci = alt_g.degrees>0
     alt_widoczne = alt_g.degrees[maska_widocznosci]
     az_widoczne = az_g.degrees[maska_widocznosci]
+    mag_widoczne = df_gwiazdy['magnitude'].values[maska_widocznosci]
+    widoczne_hip = df_gwiazdy.index.values[maska_widocznosci]
 
     r_gwiazd = (90-alt_widoczne).tolist()
     theta_gwiazd = az_widoczne.tolist()
 
-    widoczne_hip = df_gwiazdy.index.values[maska_widocznosci]
+    rozmiary_gwiazd = np.maximum(1, (5 - mag_widoczne) * 3).tolist()
     teksty_gwiazd = [nazwy_gwiazd.get(hip,"") for hip in widoczne_hip]
-
-    kolor_gwiazda = ['yellow' if tekst != "" else 'white' for tekst in teksty_gwiazd]
+    kolor_gwiazd = ['yellow' if tekst != "" else 'white' for tekst in teksty_gwiazd]
+    pelne_info_gwiazd = []
+    for i, hip in enumerate(widoczne_hip):
+        nazwa_g = nazwy_gwiazd.get(hip,"Gwiazda nienazwana")
+        jasnosc = mag_widoczne[i]
+        pelne_info_gwiazd.append(
+            f"<b>{nazwa_g}</b><br>"
+            f"Numer katalogowy: HIP {hip}<br>"
+            f"Jasność obserwowana: {jasnosc:.2f} mag"
+        )
     fig_2d_gwiazdy.add_trace(go.Scatterpolar(
         r=r_gwiazd,
         theta = theta_gwiazd,
@@ -218,12 +253,58 @@ with tab_2d_gwiazdy:
         text = teksty_gwiazd,
         textposition = 'top center',
         textfont = dict(color='yellow', size=10),
-        marker=dict(size=4, color=kolor_gwiazda,opacity=0.6),
+        marker=dict(size=rozmiary_gwiazd, color=kolor_gwiazd, opacity=0.6),
         name='Gwiazdy',
-        hoverinfo='text'
+        hovertext=pelne_info_gwiazd,
+        hovertemplate="%{hovertext}<extra></extra>"
 
     ))
+    if pokaz_kon:
+        for nazwa_kon, numer_hip in konstelacje.items():
+            r_lin = []
+            theta_lin = []
+            r_avg = []
+            theta_avg = []
 
+            for hip in numer_hip:
+                if hip in widoczne_hip:
+                    idx = np.where(widoczne_hip == hip)[0][0]
+                    r_lin.append(r_gwiazd[idx])
+                    theta_lin.append(theta_gwiazd[idx])
+                    r_avg.append(r_gwiazd[idx])
+                    theta_avg.append(theta_gwiazd[idx])
+                else:
+                    r_lin.append(None)
+                    theta_lin.append(None)
+            if any(x is not None for  x in r_lin):
+                fig_2d_gwiazdy.add_trace(go.Scatterpolar(
+                    r = r_lin,
+                    theta = theta_lin,
+                    mode = 'lines',
+                    line = dict(color='red',width=1.5,dash = 'dot'),
+                    name = nazwa_kon,
+                    hoverinfo='skip'
+            ))
+                if r_avg:
+                    r_array = np.array(r_avg)
+                    theta_rad = np.radians(theta_avg)
+                    x_pkt = r_array * np.cos(theta_rad)
+                    y_pkt = r_array * np.sin(theta_rad)
+                    x_sr = np.mean(x_pkt)
+                    y_sr = np.mean(y_pkt)
+                    r_sr = np.sqrt(x_sr**2 + y_sr**2)
+                    theta_sr = np.degrees(np.arctan2(y_sr,x_sr))
+                    theta_sr = (theta_sr + 360) % 360
+                    r_sr += 6
+                    fig_2d_gwiazdy.add_trace(go.Scatterpolar(
+                        r=[r_sr],
+                        theta=[theta_sr],
+                        mode='text',
+                        text = [f"<b>{nazwa_kon}</b>"],
+                        textfont=dict(color='lightblue',size=13),
+                        showlegend=False,
+                        hoverinfo='skip'
+                    ))
     fig_2d_gwiazdy.update_layout(
         dragmode='pan',
         polar=dict(
@@ -234,3 +315,19 @@ with tab_2d_gwiazdy:
         paper_bgcolor='black', font=dict(color='white'), height=600
     )
     st.plotly_chart(fig_2d_gwiazdy,use_container_width=True, key="wykres_2d_gwiazdy")
+    st.markdown("---")
+    st.markdown("Status konstelacji na niebie")
+    for nazwa_kon, numer_hip in konstelacje.items():
+        sklad_gwiazd = []
+        widoczne_cale = True
+
+        for hip in numer_hip:
+            nazwa_g = nazwy_gwiazd.get(hip, f"HIP {hip}")
+            if hip in widoczne_hip:
+                sklad_gwiazd.append(f"**{nazwa_g}**")
+            else:
+                sklad_gwiazd.append(f"{nazwa_g}")
+                widoczne_cale = False
+
+        st.write(f"**{nazwa_kon}** składa się z gwiazd: ")
+        st.caption(", ".join(sklad_gwiazd))
